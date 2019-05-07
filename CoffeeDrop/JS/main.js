59edb63eec55ff095ecc11f63c2f781b6e9525da
Kartik <apple@host-205-217.eduroam-local.wifi.le.ac.uk>
2019-05-07 15:01:56 +0100

    updated

---
+++ CoffeeDrop/JS/main.js
@@ -0,0 +1,212 @@
+/*--------------------- Mapp-----------------------------*/
+var map;
+function initMap() {
+     
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 54.25, lng: -3.18},
+            zoom: 6,
+            styles:[
+                     
+              {
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#f5f5f5"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.icon",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#616161"
+                  }
+                ]
+              },
+              {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                  {
+                    "color": "#f5f5f5"
+                  }
+                ]
+              },
+              {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#bdbdbd"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#eeeeee"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#757575"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#e5e5e5"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              },
+              {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#ffffff"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#757575"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#dadada"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#616161"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#e5e5e5"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#eeeeee"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                  {
+                    "color": "#c9c9c9"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#9e9e9e"
+                  }
+                ]
+              }
+
+
+
+
+            ]
+          });
+         
+        
+        }
+
+
+
+/**------------------------------Api-----------------------------------------*/
+var ajaxdata=[];
+
+$(function(){
+
+    $.ajax({
+
+        type:'GET',
+        url: 'http://coffeedrop.staging2.image-plus.co.uk/api/locations',
+        async:true,
+        dataType: "json", 
+        success: function(data){
+            
+          for(i=0;i<data.data.length;i++){
+            
+            ajaxdata.push(data.data[i]);
+            
+            var marker = new google.maps.Marker({
+              position: new google.maps.LatLng(data.data[i].coordinates.latitude,data.data[i].coordinates.longitude),
+              map: map,
+              title:data.data[i].location,
+              icon:'/Users/apple/Desktop/CoffeeDrop/CoffeeDropWebChallenge-master/Assets/images/map-pin.svg'
+            })
+
+          }
+          console.log('success',data);    
+        }
+    });
+
+});
+console.log(ajaxdata);
+
+
